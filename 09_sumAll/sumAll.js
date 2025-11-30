62ba0771fbda2558ec6e2770b0ea2339ce239966
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-11-30 19:59:30 +0900

    feat: add sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || 
+        !Number.isInteger(end) ||
+        start < 0
+    ) {
+        return "ERROR"
+    }
 
+    if (start > end) {
+        let tmp;
+        tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    let sum = 0;
+
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
