ab3d5d18f2b76029d430a5fd056ef443c741f46c
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-11-30 19:35:34 +0900

    feat: add removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    removalElements = Array.prototype.slice.call(arguments, 1)
+
+    return arr.filter(element => !removalElements.includes(element))
 };
 
 // Do not edit below this line
