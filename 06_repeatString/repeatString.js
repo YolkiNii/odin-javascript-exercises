ba3182fd43053d3ad7f895ddb0728b5c3a670d7e
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-11-30 19:18:22 +0900

    feat: add repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(s, repeatCount) {
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
 
+    let repeat = "";
+    for (let i = 0; i < repeatCount; i++) {
+        repeat += s;
+    }
+
+    return repeat;
 };
 
 // Do not edit below this line
