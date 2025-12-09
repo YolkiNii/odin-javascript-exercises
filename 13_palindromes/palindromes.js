a6f12dbdfcc09286f0661693205d43d7061370d1
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-12-08 18:37:17 -0600

    feat: complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+    const cleanWord = word.toLowerCase()
+        .split("")
+        .filter(char => alphanumeric.includes(char))
+
+    const reversed = cleanWord.toReversed();
+
+    return cleanWord.join("") === reversed.join("")
 };
 
 // Do not edit below this line
