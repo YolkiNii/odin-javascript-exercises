c00049a001ce9d03e713d8e762b5d3c3313cf89a
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-12-08 18:46:45 -0600

    feat: complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = +number;
 
+    if (number < 0)
+        return "OOPS"
+    const numbers = new Array(number + 1);
+
+    numbers[0] = 0;
+    numbers[1] = 1;
+
+    for (let i = 2; i < number + 1; i++)
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+
+    return numbers[number];
 };
 
 // Do not edit below this line
