3d499cfed6fcf59199b2a2bb9c5e8be2ab763ed4
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-12-08 23:01:10 -0600

    feat: complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((prev, person) => {
+        const currYear = new Date().getFullYear();
+        const currAge = "yearOfDeath" in person ? 
+            person.yearOfDeath - person.yearOfBirth :
+            currYear - person.yearOfBirth;
+        const prevAge = "yearOfDeath" in prev ? 
+            prev.yearOfDeath -prev.yearOfBirth :
+            currYear - prev.yearOfBirth;
 
+        return prevAge > currAge ? prev : person;
+    });
 };
 
 // Do not edit below this line
